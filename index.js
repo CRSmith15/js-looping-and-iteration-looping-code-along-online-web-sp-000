79d1462b01ff9205ce9f1f69ff386b2b0aee2920
Chance Smith <chancesmith97@hotmail.com>
2020-09-04 01:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solutions in this file
+function writeCards(person, occasion)
+  for (let i = 0, i < person.length; i++) {
+    console.log(`Thank you, ${person[i]} for the wonderful ${occasion} gift!`);
+  }
