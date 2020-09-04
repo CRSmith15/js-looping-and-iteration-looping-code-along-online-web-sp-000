fd316d366e17973fb81d18497cd7a5ba08e12ad2
Chance Smith <chancesmith97@hotmail.com>
2020-09-04 01:40:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Code your solutions in this file
-let thankYous = []
+
 
 function writeCards(person, occasion) {
   for (let i = 0; i < person.length; i++) {
-    thankYous << console.log(`Thank you, ${person[i]} for the wonderful ${occasion} gift!`);
+    [] << console.log(`Thank you, ${person[i]} for the wonderful ${occasion} gift!`);
   }
-  return thankYous
 }
